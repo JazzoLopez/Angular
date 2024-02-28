d74cee50d7111aaba727cd29fc47559946cc8a48
JazzoLopez <jazzielrodriguezlopez@gmail.com>
2024-02-28 08:20:47 -0600

    Up: llamamos al componente games para mostrarlo en index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { GamesComponent } from './games/games.component';
 
 @Component({//decorador
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet, UserComponent], //todos los modulos que utilizaremos, se refiere al nombre asi que tengamos cuidadp
+  imports: [RouterOutlet, UserComponent, GamesComponent], //todos los modulos que utilizaremos, se refiere al nombre asi que tengamos cuidadp
   templateUrl: './app.component.html',
   /* template:"<h1>hola mundo</h1>
   <app-user></app-user>" //se puede utilizar template o templateUrl*/
